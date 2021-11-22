e7c6c0fd745ebc4b7ed3d8b0a59d81d5dba7c1d0
quazar <s-bek-k@mail.ru>
2021-11-22 16:08:59 +0600

    changed form

--- index.php
+++ index.php
@@ -331,33 +331,8 @@
 
   </div>
 
-  <!-- <form id="contact" action="" method="post">
-      <h3>Quick Contact</h3>
-      <h4>Contact us today, and get reply with in 24 hours!</h4>
- 
-      <fieldset>
-        <input placeholder="Your name" name="name" type="text" tabindex="1" autofocus>
-      </fieldset>
-      <fieldset>
-        <input placeholder="Your Email Address" name="email" type="email" tabindex="2">
-      </fieldset>
-      <fieldset>
-        <input placeholder="Your Phone Number" name="tel" type="tel" tabindex="3" >
-      </fieldset>
-      <fieldset>
-        <input placeholder="Type your subject line" type="text" name="subject" tabindex="4">
-      </fieldset>
-      <fieldset>
-        <textarea name="message" placeholder="Type your Message Details Here..." tabindex="5"></textarea>
-      </fieldset>
-      <div>
-         <p class="success"> <?php echo $success;  ?></p>
-         <p class="failed"> <?php echo $failed;  ?></p>
-      </div>
-      <fieldset>
-        <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Submit Now</button>
-      </fieldset>
-    </form> -->
+
+
 
   <!-- FOOTER -->
   <footer class="container">
