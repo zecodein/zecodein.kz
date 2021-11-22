03718508c8b1dd9f52894d2ddc41f8234434ce14
quazar <s-bek-k@mail.ru>
2021-11-22 15:03:18 +0600

    added php for form email

--- index.php
+++ index.php
@@ -1,42 +1,4 @@
-<?php
-//php code to sent contact form to both client and owner
-if (isset($_POST['submit'])) {
-  $mailto = "sales@zecodein.kz"; //my email address
-  $name = $_POST['name']; //getting customer name
- $fromEmail = $_POST['email']; //getting customer email
- $phone = $_POST['tel']; //getting customer Phome number
- $subject = $_POST['subject']; //getting subject line from client
- $subject2 = "Confirmation: Message was submitted successfully"; // For customer confirmation
- 
- //Email body I will receive
- $message = "Cleint Name: " . $name . "\n"
- . "Phone Number: " . $phone . "\n\n"
- . "Client Message: " . "\n" . $_POST['message'];
-
- //Message for client confirmation
- $message2 = "Dear" . $name . "\n"
- . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
- . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
- . "Regards," . "\n" . "- HMA WebDesign";
- 
- //Email headers
- $headers = "From: " . $fromEmail; // Client email, I will receive
- $headers2 = "From: " . $mailto; // This will receive client
- 
- //PHP mailer function
- 
-  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
-  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
- 
-  //Checking if Mails sent successfully
- 
-  if ($result1 && $result2) {
-    $success = "Your Message was sent Successfully!";
-  } else {
-    $failed = "Sorry! Message was not sent, Try again Later.";
-  }
-}
-?>
+<?php include 'sentMail.php';?>
 
 <!doctype html>
 <html lang="en">
@@ -360,6 +322,34 @@ if (isset($_POST['submit'])) {
     </div>
 
   </div>
+
+  <form id="contact" action="" method="post">
+      <h3>Quick Contact</h3>
+      <h4>Contact us today, and get reply with in 24 hours!</h4>
+ 
+      <fieldset>
+        <input placeholder="Your name" name="name" type="text" tabindex="1" autofocus>
+      </fieldset>
+      <fieldset>
+        <input placeholder="Your Email Address" name="email" type="email" tabindex="2">
+      </fieldset>
+      <fieldset>
+        <input placeholder="Your Phone Number" name="tel" type="tel" tabindex="3" >
+      </fieldset>
+      <fieldset>
+        <input placeholder="Type your subject line" type="text" name="subject" tabindex="4">
+      </fieldset>
+      <fieldset>
+        <textarea name="message" placeholder="Type your Message Details Here..." tabindex="5"></textarea>
+      </fieldset>
+      <div>
+         <p class="success"> <?php echo $success;  ?></p>
+         <p class="failed"> <?php echo $failed;  ?></p>
+      </div>
+      <fieldset>
+        <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Submit Now</button>
+      </fieldset>
+    </form>
 
   <!-- FOOTER -->
   <footer class="container">
