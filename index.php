65ca0b2b7b422c804449eb8df340138014d69877
Shalqarov <Temirlan200312@gmail.com>
2022-04-18 17:09:53 +0600

    test

--- index.php
+++ index.php
@@ -10,17 +10,15 @@
     <meta name="generator" content="Hugo 0.88.1"> -->
     
 
-    <meta property="og:title" content="<zecodein>">
+    <meta property="og:title" content="zecodein">
     <meta property="og:site_name" content="zecodein">
     <meta property="og:url" content="https://www.zecodein.kz/">
     <meta property="og:description" content="Создание сайтов и веб приложений любой сложности под нужды вашего бизнеса.">
-    <meta property="og:image" content="img/z_logo3_1.svg">
+    <meta property="og:image" content="/img/z_logo3_1.svg">
     <link rel="icon" href="img/z_logo3_1.svg">
     <!-- <title>zecodein</title> -->
 
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/"> -->
-
-    
 
     <!-- Bootstrap core CSS -->
 <!-- <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet"> -->
