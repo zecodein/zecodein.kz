e5f8ad732a06281121b4c8d9c43729294a2be582
Shalqarov <Temirlan200312@gmail.com>
2022-04-24 17:48:26 +0600

    changed color

--- js/materialize.animations.js
+++ js/materialize.animations.js
@@ -13,4 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, {});
+    
 });
+document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.parallax');
+        var instances = M.Parallax.init(elems, {});
+    });
